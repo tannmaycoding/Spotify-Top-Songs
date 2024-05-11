340cd9580602de0849e394682dde7468b0d39c52
Tannmay Khandelwal <94522751+tannmaycoding@users.noreply.github.com>
2024-05-11 17:53:48 +0530

    Update spotifyTopSongs.py

--- spotifyTopSongs.py
+++ spotifyTopSongs.py
@@ -1,17 +1,5 @@
 """
 This is a command line app which can tell top songs of a given artist on Spotify.
-
-Modules Used:
-    json,
-    spotipy,
-    requests,
-    base64
-
-Functions Made:
-    getToken: Gives the token to access further functions,
-    getAuthHeader: Gives the authorization to access further functions,
-    searchArtists: searches and returns artistID of an artist,
-    getSongs: gives top songs of an artist
 """
 
 import json
